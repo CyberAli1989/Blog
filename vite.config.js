d07c9897b2c53d606ad6b7ed1503c3572cb3bf9e
CyberAli1989 <naserianali@gmail.com>
2022-11-22 22:37:48 +0330

    install laravel and change auth to username

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '127.0.0.1'
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
